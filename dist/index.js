97f54943ca9241b5346999ec773d151e3596af0d
Niccolò Maggioni <nicco.maggioni@gmail.com>
2016-07-14 01:04:00 +0200

    Dirty hack ==> Proper solution

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var nodeVersion = Number(process.version.match(/^v(\d+\.\d+)/)[1]);
-
-if (nodeVersion >= 6.2) {
-  module.exports = require('./jsondb.js'); // ES6
-} else {
-  module.exports = require('./dist/jsondb.js'); // ES5 (Babel transpiled)
-}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var nodeVersion = Number(process.version.match(/^v(\d+\.\d+)/)[1]);
+var semver = require('semver');
+var nodeVersion = semver.clean(process.version);
 
-if (nodeVersion >= 6.2) {
+if (semver.satisfies(nodeVersion, '>=6.2.2')) {
   module.exports = require('./jsondb.js');  // ES6
 } else {
   module.exports = require('./dist/jsondb.js');  // ES5 (Babel transpiled)
