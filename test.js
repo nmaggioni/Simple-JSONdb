ffec365bbe8ff0af6ccfd3580a32a7356d4d9df7
Niccolò Maggioni <nicco.maggioni@gmail.com>
2016-07-14 03:04:49 +0200

    Test suite mixin

--- test.js
+++ test.js
@@ -1,56 +1,87 @@
 var JSONdb = require('./jsondb');
 var assert = require('chai').assert;
 var fs = require('fs');
+var filePath = genFilePath();
 
 /**
  * Generates a file path and name based on current UNIX timestamp.
  * @returns {string}
  */
-function genFilePath() {
+function genFileName() {
   return '/tmp/' + Date.now().toString() + '.json';
 }
 
-// Make sure that the filePath does not exhist before starting.
-var filePath;
-while (true) {
-  try {
-    filePath = genFilePath();
-    fs.statSync(filePath);
-  } catch (err) {
-    break;
+/**
+ * Makes sure that a unique filename is generated.
+ */
+function genFilePath() {
+  while (true) {
+    try {
+      global.filePath = genFileName();
+      fs.statSync(global.filePath);
+    } catch (err) {
+      break;
+    }
   }
 }
 
-// Create a new JSONdb instance and test consistency.
-var db = new JSONdb(filePath);
-assert.instanceOf(db, JSONdb);
+/**
+ * Returns a new instance of JSONdb.
+ * @returns {JSONdb}
+ */
+function createInstance() {
+  return new JSONdb(global.filePath);
+}
 
-// Check that a non-exhistent key returns `undefined`.
-assert.typeOf(db.get('foo'), 'undefined', 'Unexpected type of initial read');
+describe("Consistency", function() {
+  it("Create a new JSONdb instance and test `instanceOf`", function() {
+    var db = createInstance();
+    assert.instanceOf(db, JSONdb);
+  });
 
-// Check that values can change.
-var change = { testVal: db.get('foo') };
-var changeFn = function() {
-  db.set('foo', new Date().toISOString());
-  change.testVal = db.get('foo');
-};
-assert.changes(changeFn, change, 'testVal', 'Values do not change');
+  it("Check that a non-exhistent key returns `undefined`", function() {
+    var db = createInstance();
+    assert.typeOf(db.get(Date.now()), 'undefined', 'Unexpected type of initial read');
+  });
+});
 
-// Check that values can change (deterministic check).
-db.set('foo', new Date().toISOString());
-var firstVal = db.get('foo');
-db.set('foo', new Date().toUTCString());
-var secondVal = db.get('foo');
-assert.notEqual(firstVal, secondVal, 'Values do not change');
+describe("Mechanics", function() {
+  it("Check that values can change", function() {
+    var db = createInstance();
+    var change = { testVal: db.get('foo') };
+    var changeFn = function() {
+      db.set('foo', Date.now());
+      change.testVal = db.get('foo');
+    };
+    assert.changes(changeFn, change, 'testVal', 'Values do not change');
+  });
 
-// Make sure that everything is written to disk.
-db.sync();
+  it("Check that values can change (deterministic)", function() {
+    var db = createInstance();
+    db.set('foo', new Date().toISOString());
+    var firstVal = db.get('foo');
+    db.set('foo', new Date().toUTCString());
+    var secondVal = db.get('foo');
+    assert.notEqual(firstVal, secondVal, 'Values do not change');
+  });
 
-// Create new instance of JSONdb to replace the old one.
-db = new JSONdb(filePath);
+  it("Verify sync to disk", function() {
+    var db = createInstance();
+    db.set('foo', Date.now());
+    assert.doesNotThrow(db.sync, Error, 'Cannot save to disk');
+  });
+});
 
-// Check that the last written value has persisted to disk and has been read successfully on reload.
-assert.equal(db.get('foo'), secondVal, 'Persistence test failed');
+describe("Persistency", function() {
+  var db = createInstance();
+  db.set('foo', Date.now());
+  var oldVal = db.get('foo');
+  db = createInstance();
+  assert.equal(db.get('foo'), oldVal, 'Reloaded value differs from last written');
+});
 
-// Cleanup.
-fs.unlinkSync(filePath);
+describe("Cleanup", function() {
+  it("Temporary file removal", function() {
+    assert.doesNotThrow(function() { fs.unlinkSync(global.filePath); }, Error, 'Unable to cleanup');
+  })
+});
