8d595fecf9bc6a61796a93ee33554c1acf23bf66
Niccolò Maggioni <nicco.maggioni@gmail.com>
2016-09-06 15:42:38 +0200

    Downwards compatibility

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var semver = require('semver');
 var nodeVersion = semver.clean(process.version);
 
-if (semver.satisfies(nodeVersion, '>=6.2.2')) {
+if (semver.satisfies(nodeVersion, '>=6.0')) {
   module.exports = require('./jsondb.js');  // ES6
 } else {
   module.exports = require('./dist/jsondb.js');  // ES5 (Babel transpiled)
