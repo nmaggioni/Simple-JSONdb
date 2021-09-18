5a823f9426b47da7ea1f649e8587e91003808a73
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-09-19 04:36:47 +0800

    Make `JSONdb` type generic

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,13 @@
-declare class JSONdb {
+declare class JSONdb<T = object> {
   constructor(filePath: string, options?: { asyncWrite?: boolean, syncOnWrite?: boolean });
   
-  set(key: string, value: object) : void;
-  get(key: string) : object | undefined;
+  set(key: string, value: T) : T;
+  get(key: string) : T | undefined;
   has(key: string) : boolean;
   delete(key: string) : boolean | undefined;
   deleteAll() : this;
   sync() : void;
-  JSON(storage?: object) : object;
+  JSON(storage?: T) : T;
 }
 
 export = JSONdb;
