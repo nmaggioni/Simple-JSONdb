9aac7cb191da74d17521920d9764563ade1fc3af
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-09-19 04:39:44 +0800

    Revert wrong typing for `JSONdb#set`

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 declare class JSONdb<T = object> {
   constructor(filePath: string, options?: { asyncWrite?: boolean, syncOnWrite?: boolean });
   
-  set(key: string, value: T) : T;
+  set(key: string, value: T) : void;
   get(key: string) : T | undefined;
   has(key: string) : boolean;
   delete(key: string) : boolean | undefined;
