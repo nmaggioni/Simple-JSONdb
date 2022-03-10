dff1e1004290652467b2a2ae027a7114a65faa70
Rahul Choubey <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2022-03-09 20:29:36 -0500

    added promise typedefs to `.sync()`

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,18 @@
 declare class JSONdb<T = any> {
-  constructor(filePath: string, options?: { asyncWrite?: boolean, syncOnWrite?: boolean, jsonSpaces?: boolean, stringify?: (o:T) => string, parse?: (s:string) => (T | undefined) });
+  constructor(filePath: string, options?: {
+    asyncWrite?: boolean,
+    syncOnWrite?: boolean,
+    jsonSpaces?: boolean,
+    stringify?: (o:T) => string,
+    parse?: (s:string) => (T | undefined)
+  });
   
   set(key: string, value: T) : void;
   get(key: string) : T | undefined;
   has(key: string) : boolean;
   delete(key: string) : boolean | undefined;
   deleteAll() : this;
-  sync() : void;
+  sync() : Promise<undefined> | undefined;
   JSON(storage?: Record<string, T>) : Record<string, T>;
 }
 
