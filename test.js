ccf1d7d48af708919a8c1e8328853578ffaf0e38
Niccolò Maggioni <nicco.maggioni@gmail.com>
2022-05-25 11:03:38 +0200

    Iterate tests multiple times

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
-var JSONdb = require('.');
-var assert = require('chai').assert;
-var fs = require('fs');
+const JSONdb = require('.');
+const assert = require('chai').assert;
+const fs = require('fs');
+
+const iterableTestsCount = 100;
 
 /**
  * Generates a file path and name based on current UNIX timestamp.
@@ -34,8 +36,7 @@ function createInstance() {
 
 describe('Privilege', function() {
   it('Tests are not being run as root', function() {
-    var isRoot = process.getuid && process.getuid() === 0;
-    //assert.isFalse(isRoot, 'Please do not run tests with root privileges!');
+    let isRoot = process.getuid && process.getuid() === 0;
     if (isRoot) {
       assert.fail(false, isRoot, 'Please do not run tests with root privileges!');
     }
@@ -48,19 +49,19 @@ describe('Consistency', function() {
   });
 
   it('Create a new JSONdb instance and test `instanceOf`', function() {
-    var db = createInstance();
+    const db = createInstance();
     assert.instanceOf(db, JSONdb);
   });
 
   it('Check error handling for paths with no access', function() {
     assert.throws(function() {
-      var db = new JSONdb('/' + Date.now().toString() + '.json', { syncOnWrite: true });
+      const db = new JSONdb('/' + Date.now().toString() + '.json', { syncOnWrite: true });
       db.set('foo', 'bar');
     });
   });
 
   it('Check that a non-exhistent key returns `undefined`', function() {
-    var db = createInstance();
+    const db = createInstance();
     assert.typeOf(db.get(Date.now()), 'undefined', 'Unexpected type of initial read');
   });
 });
@@ -71,56 +72,61 @@ describe('Mechanics', function() {
   });
 
   it('Check that values can change', function() {
-    var db = createInstance();
-    var change = { testVal: db.get('foo') };
-    var changeFn = function() {
-      db.set('foo', Date.now());
-      change.testVal = db.get('foo');
-    };
-    assert.changes(changeFn, change, 'testVal', 'Values do not change');
+    const db = createInstance();
+    for (let i = 0; i < iterableTestsCount; i++) {
+      let change = { testVal: db.get('foo') };
+      let changeFn = function() {
+        db.set('foo', Math.random());
+        change.testVal = db.get('foo');
+      };
+      assert.changes(changeFn, change, 'testVal', 'Values do not change');
+    }
   });
 
   it('Check that values can change (deterministic)', function() {
-    var db = createInstance();
-    db.set('foo', new Date().toISOString());
-    var firstVal = db.get('foo');
-    db.set('foo', new Date().toUTCString());
-    var secondVal = db.get('foo');
-    assert.notEqual(firstVal, secondVal, 'Values do not change');
+    const db = createInstance();
+    for (let i = 0; i < iterableTestsCount; i++) {
+      db.set('foo', new Date().toISOString());
+      let firstVal = db.get('foo');
+      db.set('foo', new Date().toUTCString());
+      let secondVal = db.get('foo');
+      assert.notEqual(firstVal, secondVal, 'Values do not change');
+    }
   });
 
   it('Check that keys can be deleted', function() {
-    var db = createInstance();
-    db.set('foo', Date.now());
-    var firstVal = db.get('foo');
-    db.delete('foo');
-    var secondVal = db.get('foo');
-    assert.notEqual(firstVal, secondVal, 'Values do not change');
-    assert.isUndefined(secondVal, 'Key was not deleted');
+    const db = createInstance();
+    for (let i = 0; i < iterableTestsCount; i++) {
+      db.set('foo', Date.now());
+      let firstVal = db.get('foo');
+      db.delete('foo');
+      let secondVal = db.get('foo');
+      assert.notEqual(firstVal, secondVal, 'Values do not change');
+      assert.isUndefined(secondVal, 'Key was not deleted');
+    }
   });
 
-  it('Check that keys existence can be verified (existent key)', function() {
-    var db = createInstance();
-    db.set('foo', Date.now());
-    assert.isTrue(db.has('foo'), 'Key existence is erroneous');
-  });
-
-  it('Check that keys existence can be verified (non-existent key)', function() {
-    var db = createInstance();
-    assert.isFalse(db.has('foo'), 'Key existence is erroneous');
+  it('Check that keys existence can be verified', function() {
+    const db = createInstance();
+    for (let i = 0; i < iterableTestsCount; i++) {
+      db.set('foo', Date.now());
+      assert.isTrue(db.has('foo'), 'Key existence is erroneous (returned False instead of True)');
+      db.delete('foo');
+      assert.isFalse(db.has('foo'), 'Key existence is erroneous (returned True instead of False)');
+    }
   });
 
   it('Verify sync to disk', function() {
-    var db = createInstance();
+    const db = createInstance();
     db.set('foo', Date.now());
     assert.doesNotThrow(db.sync, Error, 'Cannot save to disk');
   });
 });
 
 describe('Persistency', function() {
-  var db = createInstance();
+  let db = createInstance();
   db.set('foo', Date.now());
-  var oldVal = db.get('foo');
+  let oldVal = db.get('foo');
   db = createInstance();
   assert.equal(db.get('foo'), oldVal, 'Reloaded value differs from last written');
 });
