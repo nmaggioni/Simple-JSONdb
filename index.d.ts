4d51ee5983c6ebb3737a33611af9e062df29fa26
Qi <ziqi.qisumi@outlook.com>
2023-01-08 00:25:47 +0800

    Update index.d.ts
    fix a typo
    jsonSpaces?: boolean -> jsonSpaces?: number
    which may confuse developer who use typescript

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 declare class JSONdb<T = any> {
-  constructor(filePath: string, options?: { asyncWrite?: boolean, syncOnWrite?: boolean, jsonSpaces?: boolean, stringify?: (o:T) => string, parse?: (s:string) => (T | undefined) });
+  constructor(filePath: string, options?: { asyncWrite?: boolean, syncOnWrite?: boolean, jsonSpaces?: number, stringify?: (o:T) => string, parse?: (s:string) => (T | undefined) });
   
   set(key: string, value: T) : void;
   get(key: string) : T | undefined;
